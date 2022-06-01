517871b3e61a28103c0cfe50d27187e6cd2c703b
kunkunbebo <52291191+kunkunbebo@users.noreply.github.com>
2022-06-01 15:56:02 +1000

    Update Find_Mobile_Food_Trucks.py
    Commented print as it was used for debug in development environment

--- Find_Mobile_Food_Trucks.py
+++ Find_Mobile_Food_Trucks.py
@@ -12,4 +12,4 @@ df1=df.loc[(df['Applicant'] == 'Cochinita') & (df['locationid'] == 1591822)]
 #Below command gives an error as I still need to resolve data type issue, lat and lon is float64 which is causing a streing search on these instead of numeric value match
 #df1=df.loc[(df['Applicant'] == app) & (df['locationid'] == locid)]
 df1=df.loc[(df['Latitude'] == lat) & (df['Longitude'] == long)]
-print(df1)
+#print(df1)
