15a9a6b930805bbccab7a63763e5b97e951797ae
kunkunbebo <52291191+kunkunbebo@users.noreply.github.com>
2022-06-01 15:54:51 +1000

    Update Find_Food_Truck_Facility_SF.py
    Commented print as it was used for debug in development environment

--- Find_Food_Truck_Facility_SF.py
+++ Find_Food_Truck_Facility_SF.py
@@ -10,24 +10,24 @@ df = pd.read_csv('Mobile_Food_Facility_Permit.csv', dtype='str', sep=csv_seperat
 # Assign Latitude column to sort_lan - a new column in df with type conversion to float64 and assign latitude column to sort_lon a new column in df
 df['sort_lat']=df['Latitude'].astype('float64')
 df['sort_lon']=df['Longitude'].astype('float64')
-print("Original csv and two more columns")
-print(df)
+# print("Original csv and two more columns")
+# print(df)
 # Create new csv with less columns for easy troubleshooting of code, rest of the columns will be included in df1 once program works without errors
 df1=pd.DataFrame(columns=['locationid','lat','lon'])
-print(df1) # it will be empty dataframe as of now
+# print(df1) # it will be empty dataframe as of now
 df1['locationid']=df['locationid']
 df1['lat']=df['sort_lat']
 df1.round(2)
-print(df1['lat'])
+# print(df1['lat'])
 df1['lon']=df['sort_lon']
-print(df['sort_lon'])
-print("Subset of csv with only few columns")
-print(df1)
+# print(df['sort_lon'])
+# print("Subset of csv with only few columns")
+# print(df1)
 #User input for latitude and longitude
 newlat=input("Enter value for Latitude: ")
 newlon=input("Enter value for Longitude: ")
-print(newlat)
-print(newlon)
+# print(newlat)
+# print(newlon)
 # Fixed values for lat and lon that were used to test program   39.54,-114.20
 #newlat=39.54
 #newlon=-114.20
@@ -56,4 +56,4 @@ df1['dist']=list(map(lambda k: getDist(df1.loc[k]['lat'],df1.loc[k]['lon'],newla
 
  #This will give all locations within radius of 60 km
 df1[df1['dist']<60]
-print(df1)
+# print(df1)
